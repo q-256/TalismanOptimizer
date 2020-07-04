a70540708bf4fce5d079d011764c9395537778d9
snowball7241 <cctam03@gmail.com>
2020-07-04 14:44:15 -0700

    commented reforge stats for readability

--- Main.java
+++ Main.java
@@ -47,30 +47,31 @@ public class Main {
     }
     void generateReforgeStats(){
         //str, cd, atc spd, def, health, speed, int
-        for(int ii=0; ii<6; ii++){
-            for(int jj=0; jj<7; jj++){
+        for(int reforge=0; reforge<6; reforge++){
+            for(int stat=0; stat<7; stat++){
                 reforgeStats[ii][jj] = new int[]{0,0,0,0,0,0};
             }
         }
-        reforgeStats[0][0] = new int[]{4,5,7,10,15,20};
-
-        reforgeStats[1][0] = new int[]{2,3,4,0,0,0};
-        reforgeStats[1][1] = new int[]{2,2,2,0,0,0};
-
-        reforgeStats[2][0] = new int[]{0,0,3,5,8,12};
-        reforgeStats[2][1] = new int[]{0,0,3,5,8,12};
-        reforgeStats[2][3] = new int[]{0,0,1,2,3,4};
-
-        reforgeStats[3][0] = new int[]{1,1,1,2,3,4};
-        reforgeStats[3][1] = new int[]{3,4,5,7,10,15};
-        reforgeStats[3][2] = new int[]{0,0,1,1,1,1};
-
-        reforgeStats[4][1] = new int[]{4,5,7,10,15,20};
-
-        reforgeStats[5][0] = new int[]{0,1,0,3,0,4};
-        reforgeStats[5][1] = new int[]{0,2,0,1,0,9};
-        reforgeStats[5][2] = new int[]{0,2,0,4,0,5};
-        reforgeStats[5][3] = new int[]{0,3,0,-1,0,1};
+        //forceful
+        reforgeStats[0][0] = new int[]{4,5,7,10,15,20}; //strength
+        //superior
+        reforgeStats[1][0] = new int[]{2,3,4,0,0,0}; //strength
+        reforgeStats[1][1] = new int[]{2,2,2,0,0,0}; //crit damage
+        //strong
+        reforgeStats[2][0] = new int[]{0,0,3,5,8,12}; //strength
+        reforgeStats[2][1] = new int[]{0,0,3,5,8,12}; //crit damage
+        reforgeStats[2][3] = new int[]{0,0,1,2,3,4}; //defense
+        //itchy
+        reforgeStats[3][0] = new int[]{1,1,1,2,3,4}; //strength
+        reforgeStats[3][1] = new int[]{3,4,5,7,10,15}; //crit damage
+        reforgeStats[3][2] = new int[]{0,0,1,1,1,1}; //attack speed
+        //hurtful
+        reforgeStats[4][1] = new int[]{4,5,7,10,15,20}; //crit damage
+        //strange
+        reforgeStats[5][0] = new int[]{0,1,0,3,0,4}; //str
+        reforgeStats[5][1] = new int[]{0,2,0,1,0,9}; //crit damage
+        reforgeStats[5][2] = new int[]{0,2,0,4,0,5}; //attack speed
+        reforgeStats[5][3] = new int[]{0,3,0,-1,0,1}; //defense
 
         reforgeStats[5][4] = new int[]{0,2,0,7,0,0};
         reforgeStats[5][5] = new int[]{0,0,0,0,0,3};
